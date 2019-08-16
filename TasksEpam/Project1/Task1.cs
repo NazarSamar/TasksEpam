3ba733d54cc8dad3f045779c8fa5220bfeafc14b
Nazar Samardak <nazarsamar32@gmail.com>
2019-08-16 18:49:11 +0300

    adding validator in constructor

--- TasksEpam/Project1/Task1.cs
+++ TasksEpam/Project1/Task1.cs
@@ -26,8 +26,8 @@ namespace TasksEpam.Project1
     {
         //LT = left top 
         //RB = right bottom
-        protected Point LT { get; set; }
-        protected Point RB { get; set; }
+        public Point LT { get; set; }
+        public Point RB { get; set; }
         readonly double a;
         readonly double b;
 
@@ -37,11 +37,18 @@ namespace TasksEpam.Project1
             this.RB = new Point();
         }
         public Task1(Point left_top, Point right_bottom)
-        {
-            this.LT = left_top;
-            this.RB = right_bottom;
-            a = RB.X - LT.X;
-            b = LT.Y - RB.Y;
+        {            
+            if (left_top.X > right_bottom.X || left_top.Y < right_bottom.Y)
+            {
+                throw new Exception();
+            }
+            else
+            {
+                this.LT = left_top;
+                this.RB = right_bottom;
+                a = RB.X - LT.X;
+                b = LT.Y - RB.Y;
+            }
         }
 
         public double Perimetr() => (a+b)*2;
