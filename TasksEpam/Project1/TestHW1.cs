9e243f0d98543d5825302ccafbd65cf4bd11295e
Nazar Samardak <nazarsamar32@gmail.com>
2019-08-16 17:27:47 +0300

    Create tests to Task1

--- TasksEpam/Project1/TestHW1.cs
+++ TasksEpam/Project1/TestHW1.cs
@@ -13,6 +13,17 @@ namespace TasksEpam.Project1
     {
         //TestClass_TestMethod_ConditionAndExpectedResult 
         //TestMethod_Condition_ExpectedResult
-        
+        [TestCase]
+        public void Task1_TestPerimetr_InputIs0_ReturnTrue()
+        {
+            Task1 rectangle = new Task1(new Point(), new Point());
+            Assert.AreEqual(0, rectangle.Perimetr());
+        }
+        [TestCase]
+        public void Task1_TestPerimetr_ValidParams_ReturnTrue()
+        {
+            Task1 rectangle = new Task1(new Point(0, 4), new Point(5, 1));
+            Assert.AreEqual(16, rectangle.Perimetr());
+        }
     }
 }
