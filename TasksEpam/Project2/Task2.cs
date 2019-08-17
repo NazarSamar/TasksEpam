e9cd99ae1d180afd01c7157dea0515537b6fdb6d
Nazar Samardak <nazarsamar32@gmail.com>
2019-08-17 18:23:24 +0300

    class Figure and other classes with implementation TASK2

--- TasksEpam/Project2/Task2.cs
+++ TasksEpam/Project2/Task2.cs
@@ -1,6 +1,41 @@
-﻿namespace TasksEpam.Project2
+﻿using System;
+namespace TasksEpam.Project2
 {
-    class Task2
+    namespace Overriding
     {
+        abstract class Figure : Project2.Figure
+        {
+            readonly int X;
+            readonly int Y;
+
+            public Figure(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+
+        class Square:Figure
+        {
+            public Square(int x,int y):base(x,y)
+            {
+            }
+
+            public override void Draw()
+            {
+                Console.WriteLine(this.GetType().Name);
+            }
+        }
+        class Rectangle:Figure
+        {
+            public Rectangle(int x, int y) : base(x, y)
+            {
+            }
+
+            public override void Draw()
+            {
+                Console.WriteLine(this.GetType().Name);
+            }
+        }
     }
 }
