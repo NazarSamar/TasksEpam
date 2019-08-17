6b9b04b08b478aa0a4c4926fa0a4e25f245cffff
Nazar Samardak <nazarsamar32@gmail.com>
2019-08-17 18:34:42 +0300

    class Figure and other classes with implementation TASK3

--- TasksEpam/Project2/Task3.cs
+++ TasksEpam/Project2/Task3.cs
@@ -1,6 +1,45 @@
-﻿namespace TasksEpam.Project2
+﻿using System;
+namespace TasksEpam.Project2
 {
-    class Task3
+    namespace Virtual
     {
+        class Figure
+        {
+            readonly int X;
+            readonly int Y;
+
+            public Figure(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public virtual void Draw()
+            {
+                Console.WriteLine(this.GetType().Name);
+            }
+        }
+        class Square : Figure
+        {
+            public Square(int x, int y) : base(x, y)
+            {
+            }
+
+            public override void Draw()
+            {
+                Console.WriteLine(this.GetType().Name);
+            }
+        }
+        class Rectangle : Figure
+        {
+            public Rectangle(int x, int y) : base(x, y)
+            {
+            }
+
+            public override void Draw()
+            {
+                Console.WriteLine(this.GetType().Name);
+            }
+        }
     }
 }
