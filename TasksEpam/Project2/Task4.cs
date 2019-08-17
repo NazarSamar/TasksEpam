d6835b6d8ff310fd94cf3eb147c5931e40063a82
Nazar Samardak <nazarsamar32@gmail.com>
2019-08-17 20:28:42 +0300

    Interface IDrawable and other classes with implementation TASK4

--- TasksEpam/Project2/Task4.cs
+++ TasksEpam/Project2/Task4.cs
@@ -1,6 +1,59 @@
-﻿namespace TasksEpam.Project2
+﻿using System;
+namespace TasksEpam.Project2
 {
-    class Task4
+    namespace Interface
     {
+        interface IDrawable
+        {
+            void Draw();
+            void DrawAll(params IDrawable[] array);
+        }
+        class Figure: IDrawable
+        {
+            readonly int X;
+            readonly int Y;
+
+            public Figure()
+            {
+            }
+            public Figure(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+            public void Draw()
+            {
+                Console.WriteLine(this.GetType().Name);
+            }
+            public void DrawAll(params IDrawable[] array)
+            {
+                foreach (var item in array)
+                {
+                    item.Draw();
+                }
+            }
+        }
+        class Square : Figure
+        {
+            public Square(int x, int y) : base(x, y)
+            {
+            }
+            public new void Draw()
+            {
+                Console.WriteLine(this.GetType().Name);
+            }
+        }
+        class Rectangle : Figure, IDrawable
+        {
+            public Rectangle(int x, int y) : base(x, y)
+            {
+            }
+
+            public new void Draw()
+            {
+                Console.WriteLine(this.GetType().Name);
+            }
+        }
     }
+    
 }
