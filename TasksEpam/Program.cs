aed13537d8fdf493bde94ca3feed2249ed38d0da
Nazar Samardak <nazarsamar32@gmail.com>
2019-08-18 21:57:34 +0300

    Creating sample of menu

--- TasksEpam/Program.cs
+++ TasksEpam/Program.cs
@@ -1,18 +1,100 @@
-﻿using System;
-using TasksEpam.Project1;
-using TasksEpam.Project2;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Nazar Samardak">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace TasksEpam
 {
-    class Program
+    using System;
+    using TasksEpam.Project1;
+    using TasksEpam.Project2;    
+    using System.Collections.Generic;
+    using System.IO;
+    using Interface = TasksEpam.Project2.Interface;
+
+    public class Program
     {
-        static void Main(string[] args)
+        private static int index = 0;
+
+        private static void Main(string[] args)
         {
-            Square square = new Square();
-            Project2.Rectangle rectangle = new Project2.Rectangle();
-            square.Draw();
-            rectangle.Draw();
-            Console.ReadKey();
+            List<string> menuItems = new List<string>() {
+                "1: Taskl",
+                "2: Task2",
+                "3: Exit"
+            };
+
+            Console.CursorVisible = false;
+            while (true)
+            {
+                string selectedMenuItem = DrawMenu(menuItems);
+                switch(selectedMenuItem)
+                {
+                    case "1: Taskl":
+                        Console.WriteLine("TASK1");
+                        Console.Read();
+                        break;
+                    case "2: Task2":
+                        Console.WriteLine("TASK2");
+                        Console.Read();
+                        break;
+                    case "3: Exit":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        private static string DrawMenu(List<string> items)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (i == index)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine(items[i]);
+                }
+                else
+                {
+                    Console.WriteLine(items[i]);
+                }
+                Console.ResetColor();
+            }
+
+            ConsoleKeyInfo ckey = Console.ReadKey();
+
+            if (ckey.Key == ConsoleKey.DownArrow)
+            {
+                if (index == items.Count - 1)
+                {
+                    //index = 0; //Remove the comment to return to the topmost item in the list
+                }
+                else { index++; }
+            }
+            else if (ckey.Key == ConsoleKey.UpArrow)
+            {
+                if (index <= 0)
+                {
+                    //index = menuItem.Count - 1; //Remove the comment to return to the item in the bottom of the list
+                }
+                else { index--; }
+            }
+            else if (ckey.Key == ConsoleKey.Enter)
+            {
+                return items[index];
+            }
+            else
+            {
+                return "";
+            }
+
+            Console.Clear();
+            return "";
         }
     }
 }
