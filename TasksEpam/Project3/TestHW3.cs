4bf85ac16c46393ee291221249cf68dc47ca1e11
Nazar <nazarsamar32@gmail.com>
2019-08-27 00:37:22 +0300

    Creating tests for project3

---
+++ TasksEpam/Project3/TestHW3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksEpam.Project3
+{
+    class TestHW3
+    {
+    }
+}
